d4761d5be68435a5b8fc7a1a207515acfd262993
ballstreetclub <ceo@ballstreet.club>
2025-10-15 12:38:43 +0530

    change

--- lib/screens/favorites/favorites_screen.dart
+++ lib/screens/favorites/favorites_screen.dart
@@ -6,7 +6,7 @@ class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("Save traine coming soon")),
+      body: Center(child: Text("Save traincoming soon")),
     );
   }
 }

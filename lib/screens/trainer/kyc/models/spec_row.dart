28825cbc93026a1a6f9005c8e07c2a733f576e7d
ballstreetclub <ceo@ballstreet.club>
2025-11-20 16:21:40 +0530

    change

--- lib/screens/trainer/kyc/models/spec_row.dart
+++ lib/screens/trainer/kyc/models/spec_row.dart
@@ -4,4 +4,6 @@ class SpecRow {
   String? specialization;
   final TextEditingController certificateName = TextEditingController();
   String? certificatePhotoPath;
+  // Backend specialization proof id, if this row comes from server
+  String? proofId;
 }
